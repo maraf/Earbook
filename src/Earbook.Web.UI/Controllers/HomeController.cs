0183ad5b06608083223be264d16a36f54896fe9a
Marek Fišera <mara@neptuo.com>
2019-01-22 18:47:30 +0100

    New ear form.

--- src/Earbook.Web.UI/Controllers/HomeController.cs
+++ src/Earbook.Web.UI/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 
 namespace Earbook.Controllers
 {
@@ -21,6 +22,15 @@ namespace Earbook.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet("nove-ousko")]
+        public IActionResult NewEar() => View();
+
+        [HttpPost("nove-ousko")]
+        public IActionResult NewEar(string name, IFormFile picture)
+        {
+            return View();
         }
     }
 }
